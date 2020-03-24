13cdacfbfb21a6a5b411d7c91fd0df8e04e57073
Diego Escobedo <diego@escobedo.com>
2020-03-23 22:24:51 -0700

    Updated generate_players.py
    Made it into a proper module with documentation, and encapsulation of different parts of the ML pipeline. Also left some possible things to improve, both in individual functions and in general.

--- nn-allstars/generate_players.py
+++ nn-allstars/generate_players.py
@@ -1,22 +1,37 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
+"""
+A module that generates a list of players that meet a certain set of criteria.
 
+Functions:
+    save_dict(d,name):
+        will save a dictionary as a pickle file
+
+    load_dict(name):
+        will load a dictionary as a pickle file
+
+    prune_weird_names(st):
+        given a string, returns a copy of the string without weird characters
+
+    get_roster_stats(team, season_end_year, data_format='PER_GAME', playoffs=False):
+        will return the roster for a given season for a given team, with basic per game stats
+
+    get_player_names(start_year, end_year, minimum_mpg = 15, minimum_g = 30, verbose = False):
+        uses the get_roster_stats function to generate a list of players in specified seasons who meet certain criteria
+
+    gen(start_year, end_year, minimum_mpg = 15, minimum_g = 30, verbose = False):
+        puts it all together and generates a pickle file in the working directory with all the players
+
+Todo:
+    * maybe check if pickle file already exists, put some metadata about years so we don't replicate calls too much
+    * find some way to reduce those ugly arguments in gen
+    * all the Todos within functions
+"""
 import pandas as pd
-import numpy as np
 from requests import get
-import collections
-import matplotlib.pyplot as plt
 import pickle
-import string
-import tabloo
 
-from basketball_reference_scraper.teams import get_roster, get_team_stats, get_opp_stats, get_team_misc
-from basketball_reference_scraper.players import get_stats
+from basketball_reference_scraper.teams import get_roster
 from basketball_reference_scraper.constants import TEAM_TO_TEAM_ABBR
-from basketball_reference_scraper.utils import get_game_suffix, get_player_suffix
-
 from bs4 import BeautifulSoup
-import tensorflow as tf
-from tensorflow.keras import layers
 
 def save_dict(d,name):
     """Function to save dictionary as a pickle file in current directory, with given name.
@@ -54,6 +69,7 @@ def prune_weird_names(st):
 
     Todo:
         *not make this so hardcoded lol. I tried to do a bunch of stuff with encodings when reading the data but it just doesn't work.
+        *track new weird names if they come into NBA. Current character lists are based on 2000-2019 seasons
     """
     st = st.replace('Ã–', 'o')
     st = st.replace('Å½', 'z')
@@ -133,8 +149,8 @@ def get_roster_stats(team, season_end_year, data_format='PER_GAME', playoffs=Fal
         df = df.reset_index().drop(['Rk', 'index'], axis=1)
         return df
 
-def get_player_names(start_year, end_year, minimum_mpg = 15, minimum_g = 30):
-    """Given a range of seasons, return all the players meting the mpg and g requirements
+def get_player_names(start_year, end_year, minimum_mpg = 15, minimum_g = 30, verbose = False):
+    """Given a range of seasons, return all the players meting the minutes per game and game requirements
 
     Args:
         start year (int): start of data query
@@ -142,16 +158,18 @@ def get_player_names(start_year, end_year, minimum_mpg = 15, minimum_g = 30):
             ###Note: For a range from the 2009-2010 season to the 2013-2014 season, start_year would be 2010 and end_year would be 2014
         minimum_mpg (int): minimum minutes per game for someone to be include in this list
         minimum_g (int): minimum games for someone to be include in this list
+        verbose (bool): when True, print out when the function moves on to the next year
 
     Returns:
-        Dictionary with seasons (year the season ended) as keys and 
+        Dictionary with seasons (year the season ended) as keys and a set of players who meet the requirements as values
 
     Todo:
-        *Figure out what other data_formats you can have
+        *Figure out how **kwargs work to allow users to put unlimited filters
     """
     year_players = {}
     for year in range(start_year,end_year+1,1):
-        print(f'starting on {year}')
+        if status:
+            print(f'Status: starting on {year}')
         all_players_names = set()
         already = set()
         for team in list(TEAM_TO_TEAM_ABBR.values()):
@@ -168,5 +186,23 @@ def get_player_names(start_year, end_year, minimum_mpg = 15, minimum_g = 30):
         year_players[year] = all_players_names
     return year_players
 
-x = get_roster_stats('DAL', 2019)
-print(x['PLAYER'])
+def gen(name,start_year, end_year, minimum_mpg = 15, minimum_g = 30, verbose = False):
+    """Given a filename and a range of seasons, create a pickle file of a dictionary contianing all the players meting the minutes per game and game requirements
+
+    Args:
+        name (str): The name of the pickle file to be saved
+        start year (int): start of data query
+        end_year (int): end of data query
+            ### Note: For a range from the 2009-2010 season to the 2013-2014 season, start_year would be 2010 and end_year would be 2014
+        minimum_mpg (int): minimum minutes per game for someone to be include in this list
+        minimum_g (int): minimum games for someone to be include in this list
+        verbose (bool): when True, print out when the function moves on to the next year
+
+    Returns:
+        None
+
+    Todo:
+        * Is tehre an way to not make the arguments of this the exact same as those of the above function??
+    """
+    x = get_player_names(start_year, end_year, minimum_mpg = minimum_mpg, minimum_g = minimum_g, verbose = verbose)
+    save_dict(x,name)
